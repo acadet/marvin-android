6c364804670f90c2d78331420423356155dde68e
Adrien Cadet <acadet@live.fr>
2015-08-08 19:38:39 -0700

    Refactors base activity

---
+++ app/src/main/java/com/adriencadet/marvin/ui/activities/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.adriencadet.marvin.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.adriencadet.marvin.R;
+
+public class LandingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
